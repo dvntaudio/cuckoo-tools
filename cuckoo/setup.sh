aec4d208f1a4c5bf78e9c3e75c4ee8981252b26a
Peter Reuterås <peter@reuteras.com>
2015-06-13 23:54:43 +0200

    Starting switch to virtualbox.

--- cuckoo/setup.sh
+++ cuckoo/setup.sh
@@ -4,13 +4,13 @@
 sudo apt-get update && sudo apt-get dist-upgrade
 
 # General tools
-sudo apt-get install vim git ctags vim-doc vim-scripts
+sudo apt-get -y install vim git ctags vim-doc vim-scripts
 
 # Tools for Cuckoo
-sudo apt-get install python python-dev python-sqlalchemy python-bson \
+sudo apt-get -y install python python-dev python-sqlalchemy python-bson \
     python-dpkt python-jinja2 python-magic python-pymongo python-gridfs \
-    python-libvirt python-bottle python-pefile python-chardet qemu-kvm \
-    libvirt-bin bridge-utils tcpdump libcap2-bin python-pip virt-manager \
+    python-bottle python-pefile python-chardet libqt4-network \
+    bridge-utils tcpdump libcap2-bin python-pip libqt4-opengl\
     libtool automake autoconf libfuzzy2 libfuzzy-dev libxml2 libxslt1-dev \
     postgresql-server-dev-9.4 curl libcurl4-gnutls-dev
 
@@ -25,11 +25,17 @@ if [ ! -d ~/src ]; then
     git clone https://github.com/cuckoobox/cuckoo.git
     wget -O ssdeep-2.13.tar.gz http://sourceforge.net/projects/ssdeep/files/ssdeep-2.13/ssdeep-2.13.tar.gz/download
     tar xvfz ssdeep-2.13.tar.gz
+    wget -O virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb \
+        http://download.virtualbox.org/virtualbox/4.3.28/virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb
+    sudo dpkg --install virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb
 fi
 
 # Setup for Cuckoo
 if [ ! -d "/home/cuckoo" ]; then
     sudo adduser cuckoo
 fi
-sudo usermod -G libvirt cuckoo
+sudo usermod -G vboxusers cuckoo
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+
+sudo apt-get -y install linux-headers-$(uname -r)
+sudo /etc/init.d/vboxdrv setup
