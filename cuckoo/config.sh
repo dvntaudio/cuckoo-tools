9b4e1ab61a5cb58fae230626dc58871297ee64f2
Peter Reuterås <peter@reuteras.com>
2015-06-13 21:18:06 +0200

    Missed /

--- cuckoo/config.sh
+++ cuckoo/config.sh
@@ -3,6 +3,6 @@
 ROOTDIR=~/src/cuckoo/conf
 HOSTIP=$(/bin/hostname -I)
 
-sed -i -e "s/machinery = virtualbox/machinery = kvm" $ROOTDIR/cuckoo.conf
+sed -i -e "s/machinery = virtualbox/machinery = kvm/" $ROOTDIR/cuckoo.conf
 sed -i -e "s/ip = 192.168.56.1/ip = $HOSTIP/" $ROOTDIR/cuckoo.conf
 sed -i -e "s/interface = vboxnet/interface = virbr0/" $ROOTDIR/auxiliary.conf
