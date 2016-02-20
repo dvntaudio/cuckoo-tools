5ccfc86cdf76a9541bfb5abed3f1dc1a58d61c66
Peter Reuterås <peter@reuteras.com>
2016-02-20 20:47:23 +0100

    Start move to Cuckoo 2.0 RC1.

--- README.md
+++ README.md
@@ -3,3 +3,20 @@ cuckoo-tools
 
 Tools and configs for my Kali installation.
 
+Setup
+=====
+
+I tested this script on Debian 8.3
+
+    sudo apt-get install git
+    git clone 
+
+Start Cuckoo:
+
+    cd ~/src/cuckoo
+    ./cuckoo.py
+
+Start Cuckoo web interface at http://127.0.0.1:8000/:
+
+    cd ~/src/cuckoo/web
+    python manage.py runserver
--- bin/setup.sh
+++ bin/setup.sh
@@ -6,36 +6,34 @@ sudo apt-get update && sudo apt-get dist-upgrade
 # General tools
 sudo apt-get -y install vim git ctags vim-doc vim-scripts
 
-# Tools for Cuckoo
-sudo apt-get -y install python python-dev python-sqlalchemy python-bson \
-    python-dpkt python-jinja2 python-magic python-pymongo python-gridfs \
-    python-bottle python-pefile python-chardet libqt4-network \
-    bridge-utils tcpdump libcap2-bin python-pip libqt4-opengl\
-    libtool automake autoconf libfuzzy2 libfuzzy-dev libxml2 libxslt1-dev \
-    postgresql-server-dev-9.4 curl libcurl4-gnutls-dev
+# Tools for Vmware
+sudo apt-get install open-vm-tools-desktop fuse
 
-# Install globaly
-sudo pip install django maec \
-    git+https://github.com/kbandla/pydeep#egg=pydeep
+# Tools for Cuckoo
+sudo apt-get install python python-pip python-dev libffi-dev libssl-dev \
+    mongodb qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils \
+    python-libvirt tcpdump libcap2-bin
 
 # Get volatility and cuckoo
 if [ ! -d ~/src ]; then
     mkdir ~/src && cd ~/src
     git clone https://github.com/volatilityfoundation/volatility.git
     git clone https://github.com/cuckoobox/cuckoo.git
-    wget -O ssdeep-2.13.tar.gz http://sourceforge.net/projects/ssdeep/files/ssdeep-2.13/ssdeep-2.13.tar.gz/download
-    tar xvfz ssdeep-2.13.tar.gz
-    wget -O virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb \
-        http://download.virtualbox.org/virtualbox/4.3.28/virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb
-    sudo dpkg --install virtualbox-4.3_4.3.28-100309~Debian~wheezy_amd64.deb
 fi
+
+#    wget -O ssdeep-2.13.tar.gz http://sourceforge.net/projects/ssdeep/files/ssdeep-2.13/ssdeep-2.13.tar.gz/download
+#    tar xvfz ssdeep-2.13.tar.gz
+
+# Install globaly
+#sudo pip install django maec \
+#    git+https://github.com/kbandla/pydeep#egg=pydeep
 
 # Setup for Cuckoo
 if [ ! -d "/home/cuckoo" ]; then
     sudo adduser cuckoo
 fi
-sudo usermod -G vboxusers cuckoo
+
+# Let the cuckoo user access tcpdump
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 
 sudo apt-get -y install linux-headers-$(uname -r)
-sudo /etc/init.d/vboxdrv setup
