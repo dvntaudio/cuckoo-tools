a6d4a4e5f5f4771c78c2fb1b6828cc7408290635
Peter Reuterås <peter@reuteras.com>
2016-02-20 21:48:14 +0100

    Update config configuration.

--- bin/config.sh
+++ bin/config.sh
@@ -3,4 +3,7 @@
 ROOTDIR=~/src/cuckoo/conf
 HOSTIP=$(/bin/hostname -I)
 
+
+sed -i -e "s/machinery = virtualbox/machinery = kvm/" $ROOTDIR/cuckoo.conf
 sed -i -e "s/ip = 192.168.56.1/ip = $HOSTIP/" $ROOTDIR/cuckoo.conf
+sed -i -e "s/upload_max_size = 10465760/upload_max_size = 52428800/" $ROOTDIR/cuckoo.conf
