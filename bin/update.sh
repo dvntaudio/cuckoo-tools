f66ff1660ebe0acb5c9b9499b5ccd80119be959d
Peter Reuterås <peter@reuteras.com>
2016-02-21 19:59:29 +0100

    Remove old code for now.

--- bin/update.sh
+++ bin/update.sh
@@ -3,25 +3,3 @@
 # Update Debian
 sudo apt-get update && sudo apt-get dist-upgrade
 
-tempfoo=$(basename $0)
-TMPFILE=$(mktemp /tmp/${tempfoo}.XXXXXX) || exit 1
-
-# Update Python packages
-/usr/bin/pip freeze | cut -f1 -d= | \
-    egrep -v "(git-remote-helpers|Brlapi|GnuPGInterface|Magic-file-extensions|apt-xapian-index|dpkt|reportbug)" > $TMPFILE
-/usr/bin/pip install --upgrade -r $TMPFILE
-rm -f $TMPFILE
-
-# Update Cuckoo
-cd ~/src/cuckoo/conf
-test -d ../../conf/cuckoo/ || mkdir -p ../../conf/cuckoo/
-cp auxiliary.conf cuckoo.conf kvm.conf reporting.conf ../../conf/cuckoo/
-git reset ORIG_HEAD --hard
-git pull
-cp ../../conf/cuckoo/* .
-
-# Update Volatility
-cd ~/src/volatility
-git pull
-sudo make install
-
