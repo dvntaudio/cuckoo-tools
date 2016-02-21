8c5c066362ccfdec80764034bffb1a64422e0a9c
Peter Reuterås <peter@reuteras.com>
2016-02-21 12:12:51 +0100

    Config for mitmproxy cert

--- bin/config.sh
+++ bin/config.sh
@@ -3,7 +3,14 @@
 ROOTDIR=~/src/cuckoo/conf
 HOSTIP=$(/bin/hostname -I)
 
-
-sed -i -e "s/machinery = virtualbox/machinery = kvm/" $ROOTDIR/cuckoo.conf
 sed -i -e "s/ip = 192.168.56.1/ip = $HOSTIP/" $ROOTDIR/cuckoo.conf
-sed -i -e "s/upload_max_size = 10465760/upload_max_size = 52428800/" $ROOTDIR/cuckoo.conf
+
+mitmproxy > /dev/null 2>&1 & 
+
+MITM_PID=$!
+
+sleep 10
+
+kill -9 $MITM_PID
+
+cp ~/.mitmproxy/mitmproxy-ca-cert.p12 ~/src/cuckoo/analyzer/windows/bin/cert.p12
