29eeade36134158612b23e3a4a7c14b2214fde5d
Peter Reuterås <peter@reuteras.com>
2016-02-21 10:23:25 +0100

    Four hours of sleep...

--- bin/start.sh
+++ bin/start.sh
@@ -6,5 +6,5 @@ if [ !  -z "$MOUNTP" ]; then
     if [ ! -d ~/shared ]; then
         mkdir ~/shared
     fi
-    sudo mount -t vmhgfs .host:/MOUNTP $HOME/shared
+    sudo mount -t vmhgfs .host:/$MOUNTP $HOME/shared
 fi    
