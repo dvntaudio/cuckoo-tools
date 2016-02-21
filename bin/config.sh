2b0df7d8f6265cd2c1fb7399e5e40e2d65a8bd15
Peter Reuterås <peter@reuteras.com>
2016-02-21 19:33:41 +0100

    Configure cuckoo.

--- bin/config.sh
+++ bin/config.sh
@@ -3,7 +3,10 @@
 ROOTDIR=~/src/cuckoo/conf
 HOSTIP=$(/bin/hostname -I)
 
+cp ~/cuckoo-tools/files/*.conf $ROOTDIR
 sed -i -e "s/ip = 192.168.56.1/ip = $HOSTIP/" $ROOTDIR/cuckoo.conf
+
+sudo cp ~/cuckoo-tools/files/suricata.yaml /etc/suricata
 
 mitmproxy > /dev/null 2>&1 & 
 
