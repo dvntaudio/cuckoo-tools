c3781a26586cd44bc35e2ac69d677b6189c93315
Peter Reuterås <peter@reuteras.com>
2016-02-21 09:23:59 +0100

    Use -qq with apt-get. Add needed sudo.

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,22 +4,22 @@
 sudo apt-get update && sudo apt-get dist-upgrade
 
 # General tools
-sudo apt-get -y install vim git ctags vim-doc vim-scripts
+sudo apt-get -y -qq install vim git ctags vim-doc vim-scripts
 
 # Tools for Vmware
-sudo apt-get -y install open-vm-tools-desktop fuse
+sudo apt-get -y -qq install open-vm-tools-desktop fuse
 
 # Tools for Cuckoo
-sudo apt-get -y install python python-pip python-dev libffi-dev libssl-dev \
+sudo apt-get -y -qq install python python-pip python-dev libffi-dev libssl-dev \
     mongodb qemu-kvm libvirt-bin bridge-utils yara python-yara libyara3 \
     libyara-dev python-libvirt tcpdump libcap2-bin virt-manager swig \
-    suricata tesseract-ocr
+    suricata tesseract-ocr libjpeg-dev linux-headers-$(uname -r)
 
 # Configure suricata
 if [ ! -e /etc/suricata/suricata.yaml ]; then
     sudo cp files/suricata.yaml /etc/suricata/suricata.yaml
-    chown root:root /etc/suricata/suricata.yaml
-    chmod 644 /etc/suricata/suricata.yaml
+    sudo chown root:root /etc/suricata/suricata.yaml
+    sudo chmod 644 /etc/suricata/suricata.yaml
 fi
 
 # Install python packages globaly
@@ -40,11 +40,6 @@ if [ ! -d ~/src ]; then
     cd
 fi
 
-#    wget -O ssdeep-2.13.tar.gz http://sourceforge.net/projects/ssdeep/files/ssdeep-2.13/ssdeep-2.13.tar.gz/download
-#    tar xvfz ssdeep-2.13.tar.gz
-
-#    git+https://github.com/kbandla/pydeep#egg=pydeep
-
 # Setup for Cuckoo
 if [ ! -d "/home/cuckoo" ]; then
     sudo adduser cuckoo
@@ -54,4 +49,3 @@ fi
 # Let the cuckoo user access tcpdump
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 
-sudo apt-get -y install linux-headers-$(uname -r)
