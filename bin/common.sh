9a69855d756b2307ce727d69aeedad7a7b872446
Peter Reuterås <peter@reuteras.com>
2016-03-19 10:26:14 +0100

    Move update rules to common.sh.

---
+++ bin/common.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function update_rules(){
+    echo -n "Updating rules. "
+    RULESDIR=$(mktemp -d)
+    cd $RULESDIR
+    wget -q http://rules.emergingthreats.net/open/suricata/emerging.rules.tar.gz
+    tar xfz emerging.rules.tar.gz
+    sudo mv rules/* /etc/suricata/rules/
+    rm -rf $RULESDIR
+    echo "Done."
+}
+
